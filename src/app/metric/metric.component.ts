acfc6f57408ae4d40c23ac6d7639059e5f2519e2
Jeremy Wilken <gnomation@gnomeontherun.com>
2017-01-02 13:53:56 -0600

    use onPush and ngOnChanges

--- src/app/metric/metric.component.ts
+++ src/app/metric/metric.component.ts
@@ -1,29 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-metric',
   templateUrl: './metric.component.html',
-  styleUrls: ['./metric.component.css']
+  styleUrls: ['./metric.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MetricComponent {
-  private _value: number = 0;
-  private _max: number = 100;
+export class MetricComponent implements OnChanges {
+  @Input('used') value: number = 0;
+  @Input('available') max: number = 100;
 
-  @Input('used')
-  set value(value: number) {
-    if (isNaN(value)) value = 0;
-    this._value = value;
+  ngOnChanges(changes) {
+    if (changes.value && isNaN(changes.value.currentValue)) this.value = 0;
+    if (changes.max && isNaN(changes.max.currentValue)) this.max = 0;
   }
-
-  get value(): number { return this._value; }
-
-  @Input('available')
-  set max(max: number) {
-    if (isNaN(max)) max = 100;
-    this._max = max;
-  }
-
-  get max(): number { return this._max; }
 
   isDanger() {
     return this.value / this.max > 0.7;
--- src/app/nodes-row/nodes-row.component.ts
+++ src/app/nodes-row/nodes-row.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: '[app-nodes-row]',
   templateUrl: './nodes-row.component.html',
-  styleUrls: ['./nodes-row.component.css']
+  styleUrls: ['./nodes-row.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NodesRowComponent {
   @Input() node: any;
