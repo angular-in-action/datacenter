7dd577b7b34e96cd9e1ef4a3023b8f892e730d87
Jeremy Wilken <gnomation@gnomeontherun.com>
2017-01-03 11:02:52 -0600

    output event and template variable

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent  {
 
-  constructor() { }
+  @Output() onRefresh: EventEmitter<null> = new EventEmitter<null>();
 
-  ngOnInit() {
+  refresh() {
+    this.onRefresh.emit();
   }
 
 }
