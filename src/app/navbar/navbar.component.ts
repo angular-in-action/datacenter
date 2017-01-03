ce7a63ad4413c6c2e6d73076f48cb5fee081c952
Jeremy Wilken <gnomation@gnomeontherun.com>
2017-01-03 11:06:16 -0600

    css styling

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  styles: [`.btn { background-color: #999999; }`],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class NavbarComponent  {
 
