9a3149ac40b08c043f2458c9170b6ed9f5622a01
Jeremy Wilken <gnomation@gnomeontherun.com>
2017-04-25 15:57:54 -0500

    intercept inputs

--- src/app/metric/metric.component.ts
+++ src/app/metric/metric.component.ts
@@ -8,8 +8,24 @@ import { Component, Input } from '@angular/core';
 export class MetricComponent {
   @Input() title: string;
   @Input() description: string;
-  @Input('used') value: number;
-  @Input('available') max: number;
+  private _value: number = 0;
+  private _max: number = 100;
+
+  @Input('used')
+  set value(value: number) {
+    if (isNaN(value)) value = 0;
+    this._value = value;
+  }
+
+  get value(): number { return this._value; }
+
+  @Input('available')
+  set max(max: number) {
+    if (isNaN(max)) max = 100;
+    this._max = max;
+  }
+
+  get max(): number { return this._max; }
 
   isDanger() {
     return this.value / this.max > 0.7;
